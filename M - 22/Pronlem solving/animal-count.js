2cf55678f2e43a20071b320784df60cae5da543b
asadulR <asad1252001@gmail.com>
2022-02-02 09:42:46 +0600

    Problems solveing

---
+++ M - 22/Pronlem solving/animal-count.js
@@ -0,0 +1,45 @@
+/*
+    first 10 miles = 10 animal / mile
+    second 10 miles = 50 animal / mile
+    after 20 miles = 100 animal / mile
+
+    total animal seen in miles....
+*/
+
+function animalCount(miles) {
+    const animalDensityFirst10Miles = 10;
+    const animalDensitySecond10Miles = 50;
+    const animalDensityAfter20Miles = 100;
+    if(miles <= 10) {
+        const count = miles * animalDensityFirst10Miles;
+
+        return count;
+    }
+    else if(miles <= 20) {
+        const first10Animal = 10 * animalDensityFirst10Miles;
+
+        const restMiles = miles - 10;
+        const second10Animal = restMiles * animalDensitySecond10Miles;
+
+        const totalAnimal = first10Animal + second10Animal;
+        return totalAnimal;
+    }
+    else{
+        const first10Animal = 10 * animalDensityFirst10Miles;
+        const second10Animal = 10 * animalDensitySecond10Miles;
+
+        const restMiles = miles - 20;
+
+        const restDensAnimals = restMiles * animalDensityAfter20Miles;
+
+        const totalAnimal = first10Animal + second10Animal + restDensAnimals;
+
+        return totalAnimal;
+    }
+}
+
+
+
+const animals = animalCount(30);
+
+console.log(animals);
---
+++ M - 22/Pronlem solving/total-price.js
@@ -0,0 +1,79 @@
+// const products = [
+//     {name: 'Laptop', price: 55000, quantity: 1},
+//     {name: 'Shirt', price: 500, quantity: 8},
+//     {name: 'Mobile', price: 23000, quantity: 1},
+//     {name: 'Watch', price: 5000, quantity: 4}
+// ];
+
+
+// function totalPrice(products){
+//     let totalPrice = 0;
+
+//     for(const product of products){
+//         totalPrice = totalPrice + product.price;
+//     }
+//     return totalPrice;
+// }
+// const productsPrice = totalPrice(products);
+// console.log(productsPrice);
+
+
+
+
+
+//  Mega shopping
+
+
+const products = [
+    {name: 'Laptop', price: 55000, quantity: 1},
+    {name: 'Shirt', price: 500, quantity: 8},
+    {name: 'Mobile', price: 23000, quantity: 1},
+    {name: 'Watch', price: 5000, quantity: 4}
+];
+
+
+function totalPrice(cart){
+    let cartTotalPrice = 0;
+    
+
+    for(const product of cart){
+        let productTotal = product.price * product.quantity;
+
+        cartTotalPrice = cartTotalPrice + productTotal;
+
+    }
+    return cartTotalPrice;
+}
+const productsPrice = totalPrice(products);
+console.log(productsPrice);
+
+
+
+
+
+
+
+
+
+const shoppingProducts = [
+    {name: 'Laptop', price: 55000, quantity: 1},
+    {name: 'Shirt', price: 500, quantity: 8},
+    {name: 'Pant', price: 800, quantity: 4},
+    {name: 'Mobile', price: 23000, quantity: 4},
+    {name: 'Watch', price: 5000, quantity: 4}
+];
+
+
+function totalPrice(cart){
+    let cartTotalPrice = 0;
+
+    for(const product of cart) {
+        totalProductPrice = product.price * product.quantity;
+
+        cartTotalPrice = totalProductPrice + cartTotalPrice;
+    }
+    return cartTotalPrice;
+}
+   
+const shoppingProductsPrice = totalPrice(shoppingProducts);
+console.log(shoppingProductsPrice);
